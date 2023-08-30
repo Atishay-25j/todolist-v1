c55da470fe6da145eb7d483119f44117e13ecc07
atishay_25 <codesome.aj.25@gmail.com>
2023-08-30 16:42:35 +0530

    hnjikkl

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express  = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname+"/date.js");
-let items = ["cook food" , "Bathing" , "washing"];
+let items = ["Add To Do Work Below"];
 let newitems = [];
 const app = express();
 app.set('view engine','ejs');
